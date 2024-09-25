2dfcd2fe0cec8c25344196bbe2dc7009344f25c2
richard Iyevhobu <richardIyevhobu@gmail.com>
2024-09-25 23:55:49 +0100

    add

--- src/main/java/guru/springboot/spring_boot_6/domain/Author.java
+++ src/main/java/guru/springboot/spring_boot_6/domain/Author.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private String firstName;
     private String lastName;
 
@@ -21,6 +22,9 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
 
     public String getFirstName() {
         return firstName;
@@ -36,5 +40,28 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+
+        return getId() == author.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
     }
 }
